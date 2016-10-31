246eeec2ee3fcee8f249320c3a76782585117229
holine <root@holine.net>
2016-10-31 11:03:07 +0800

    修改窗口尺寸，已避免滚动条的出现

--- WeChat/scripts/background.js
+++ WeChat/scripts/background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('html/WeChat.html', {
 		'bounds' : {
 			'width' : 1000,
-			'height' : 700
+			'height' : 732
 		},
 		'frame' : 'none',
 		'resizable' : false
